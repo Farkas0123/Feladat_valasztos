b169de51ad6c793ac105848058b4511b6c03d4a4
Farkas0123 <ukosfarkas@gmail.com>
2022-04-19 14:03:04 +0200

    Első lépések

---
+++ Feladat_valaszt/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Feladat_valaszt project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Feladat_valaszt.settings')
+
+application = get_asgi_application()
